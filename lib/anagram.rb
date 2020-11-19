e32b7e186edd7311fbf312ad74691a015d924b4f
Naomi-Rhames <naomirhames2020@gmail.com>
2020-11-19 02:53:46 -0600

    commit tree 0c871efdb51ac689250828c2a8a77622db2118cd

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :letter
+
+def initialize(letter)
+    @letter = letter
+ end
+
+def match(string)
+    string.select do |i|
+        i.split("").sort == letter.split("").sort
+    end
+end
+
+end
+ 
+
